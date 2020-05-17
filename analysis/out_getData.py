bc7a6b0fedf4549aa49609eba5972955718cba5e
smeetdc <55862771+smeetdc@users.noreply.github.com>
2020-05-17 03:14:57 -0400

    city data function

--- analysis/out_getData.py
+++ analysis/out_getData.py
@@ -41,3 +41,30 @@ def getComplainData():
 
     return complain_data[col].values.tolist()
 
+
+def getCityData():
+    # read city data 
+    city_data = pd.read_csv('data/processed/party_in_nyc.csv')
+    city_data = city_data.dropna()
+
+    # needed columns
+    col = ['City']
+    city_data = city_data[col].values.tolist()
+    new_city_data = list()
+    for i in range(len(city_data)):
+        temp = city_data[i][0]
+        new_city_data.append(temp)
+
+    keys = Counter(new_city_data).keys()
+    values = Counter(new_city_data).values()
+    c = zip(*sorted(zip(values, keys)))
+    keys, values = [list(tuple) for tuple in c]
+    keys = keys[::-1]
+    values = values[::-1]
+    needed_keys = keys[:6]
+    needed_values = values[:6]
+    f_list = list()
+    for i in range(6):
+        f_list.append([needed_keys[i], needed_values[i]])
+
+    return f_list
